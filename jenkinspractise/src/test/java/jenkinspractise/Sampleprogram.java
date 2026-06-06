db4b65296c442b05ea7f89b413bfbf08282892c3
Suresh-SK96 <sureshtest829@gmail.com>
2026-06-06 17:50:58 +0530

    New file

---
+++ jenkinspractise/src/test/java/jenkinspractise/Sampleprogram.java
@@ -0,0 +1,14 @@
+package jenkinspractise;
+
+import org.testng.annotations.Test;
+
+public class Sampleprogram {
+
+	@Test
+    public void testLogin() {
+
+        System.out.println("Login Test Executed");
+
+    }
+
+}
